922411a2fd69b746ad2e3ad47f6b991c7b40b6f9
김성규 <link5658@gmail.com>
2020-11-23 20:06:57 +0900

    feat: Add value to graph from nrf in real-time

--- app/src/main/java/com/seoultech/ecgmonitor/monitor/MonitorActivity.kt
+++ app/src/main/java/com/seoultech/ecgmonitor/monitor/MonitorActivity.kt
@@ -4,10 +4,13 @@ import android.bluetooth.BluetoothDevice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.seoultech.ecgmonitor.R
 import com.seoultech.ecgmonitor.databinding.ActivityMonitorBinding
 import com.seoultech.ecgmonitor.extension.obtainViewModel
+import kotlinx.android.synthetic.main.activity_monitor.*
 
 class MonitorActivity : AppCompatActivity() {
 
@@ -18,11 +21,32 @@ class MonitorActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMonitorBinding
     private lateinit var monitorViewModel: MonitorViewModel
+    private var isConnected = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_monitor)
-        monitorViewModel = obtainViewModel()
+        monitorViewModel = obtainViewModel().apply {
+            isConnected.observe(this@MonitorActivity, {
+                if (it) {
+                    this@MonitorActivity.isConnected = true
+                    Toast.makeText(this@MonitorActivity, "Connected", Toast.LENGTH_SHORT).show()
+                } else {
+                    if (this@MonitorActivity.isConnected) {
+                        this@MonitorActivity.isConnected = false
+                        Toast.makeText(this@MonitorActivity, "Disconnected", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            })
+            receivedValue.observe(this@MonitorActivity) {
+                binding.ecggraphMonitor.addValue(it.toDouble())
+            }
+            isFailure.observe(this@MonitorActivity) {
+                if (it) {
+                    Toast.makeText(this@MonitorActivity, "Fail", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
         val device = intent.getParcelableExtra<BluetoothDevice>(EXTRA_DEVICE)
         if (device == null) {
             Log.e(TAG, "device is null")
